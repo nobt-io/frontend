2f400076851ccf22848d9ce479c770313aa3bfd7
duffleit <davidleitner@live.at>
2016-09-01 00:07:42 +0200

    added Create Route

---
+++ src/routes/Create/components/Create.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Create.scss'
+
+export const Create = () => (
+  <div className={classes['Create']}>
+    <h4>Create</h4>
+  </div>
+)
+
+export default Create
