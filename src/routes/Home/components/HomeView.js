5c6388ea7a987e51a782e34019c2271942a4f72e
duffleit <davidleitner@live.at>
2016-08-31 22:55:18 +0200

    updated home view to only include nobt.io title

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,14 +1,9 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
 import classes from './HomeView.scss'
 
 export const HomeView = () => (
   <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className={classes.duck}
-      src={DuckImage} />
+    <h4>Nobt.io</h4>
   </div>
 )
 
