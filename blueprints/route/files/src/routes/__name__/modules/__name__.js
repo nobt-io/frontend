84258b06ed3196cf42f440da391fba7f316f940d
duffleit <davidleitner@live.at>
2016-09-07 19:46:52 +0200

    updated blueprints

--- blueprints/route/files/src/routes/__name__/modules/__name__.js
+++ blueprints/route/files/src/routes/__name__/modules/__name__.js
@@ -1,55 +1,23 @@
-// ------------------------------------
-// Constants
-// ------------------------------------
-export const COUNTER_INCREMENT = '<%= pascalEntityName %>.COUNTER_INCREMENT'
+export const actionNames = {
+  ACTION_NAME: '<%= pascalEntityName %>.ACTION_NAME'
+}
 
-// ------------------------------------
-// Actions
-// ------------------------------------
-export function increment (value = 1) {
-  return {
-    type: COUNTER_INCREMENT,
-    payload: value
+const initialState = { }
+
+export const actionCreator = {
+  actionCreatorOne: function(value){
+    return {
+      type: actionNames.ACTION_NAME,
+      payload: value
+    }
   }
 }
 
-/*  This is a thunk, meaning it is a function that immediately
-    returns a function for lazy evaluation. It is incredibly useful for
-    creating async actions, especially when combined with redux-thunk!
-
-    NOTE: This is solely for demonstration purposes. In a real application,
-    you'd probably want to dispatch an action of COUNTER_DOUBLE and let the
-    reducer take care of this logic.  */
-
-export const doubleAsync = () => {
-  return (dispatch, getState) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        dispatch(increment(getState().counter))
-        resolve()
-      }, 200)
-    })
-  }
+const actionHandlers = {
+  [ACTION_NAME]: (state, action) => state
 }
 
-export const actions = {
-  increment,
-  doubleAsync
-}
-
-// ------------------------------------
-// Action Handlers
-// ------------------------------------
-const ACTION_HANDLERS = {
-  [COUNTER_INCREMENT]: (state, action) => state + action.payload
-}
-
-// ------------------------------------
-// Reducer
-// ------------------------------------
-const initialState = 0
-export default function counterReducer (state = initialState, action) {
-  const handler = ACTION_HANDLERS[action.type]
-
+export default function <%= camelEntityName %>Reducer (state = initialState, action) {
+  const handler = actionHandlers[action.type]
   return handler ? handler(state, action) : state
 }
