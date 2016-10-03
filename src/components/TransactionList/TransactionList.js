76d631ddefb06a179f35ac059b4a6e9ddf77a00c
duffleit <davidleitner@live.at>
2016-10-03 17:12:44 +0200

    added TransactionList

---
+++ src/components/TransactionList/TransactionList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './TransactionList.scss'
+
+export const TransactionList = () => (
+  <div className={styles.TransactionList}>
+    <h1>TransactionList</h1>
+  </div>
+)
+
+export default TransactionList
