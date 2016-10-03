6eab13e96cc09c44d0b16b48320052ee41f8fa60
duffleit <davidleitner@live.at>
2016-10-03 17:12:36 +0200

    added Nobt Component Tab Styling

--- src/routes/Nobt/components/Nobt.js
+++ src/routes/Nobt/components/Nobt.js
@@ -4,6 +4,7 @@ import styles from './Nobt.scss'
 import Button from 'components/Button';
 import Header from 'components/Header'
 import NobtSummary from 'components/NobtSummary'
+import TransactionList from 'components/TransactionList'
 import {Tab, Tabs} from 'react-toolbox';
 
 export const Nobt = React.createClass({
@@ -15,15 +16,20 @@ export const Nobt = React.createClass({
 
   render: function () {
     return (
-      <div className={styles.Nobt}>
+      <div className={styles.nobt}>
         <Header showButton={true}>
           <Button className={styles.button} icon="add_box" onClick={this.createNobt}>Add Nobt</Button>
         </Header>
         <NobtSummary nobtName={this.props.name} total={this.props.total} member={this.props.member}/>
-        <Tabs index={this.props.tabIndex} onChange={this.props.changeTab} fixed>
-          <Tab label="Transactions"><small>First Content</small></Tab>
-          <Tab label="Expenses"><small>Second Content</small></Tab>
-        </Tabs>
+        <div>
+          <Tabs
+            theme={{pointer: styles.pointer, tabs: styles.tabs}}
+            index={this.props.tabIndex}
+            onChange={this.props.changeTab} fixed>
+            <Tab label="Transactions"><TransactionList></TransactionList></Tab>
+            <Tab label="Expenses"><small>Second Content</small></Tab>
+          </Tabs>
+        </div>
       </div>
     );
   }
