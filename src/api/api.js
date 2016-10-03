9308f3f7e83980475fc4b94a691a22222037aee9
Thomas Eizinger <thomas@nobt.io>
2016-10-03 16:15:03 +0200

    add logging to api

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
+import debug from 'debug';
 
 const requestConfig = {headers: {'Content-Type': 'application/json; charset=UTF-8'}};
-const apiBase =  "http://localhost:8080/";
+const apiBase = "http://localhost:8080/";
+
+var log = (namespace) => (value) => {
+  debug(namespace)(value);
+  return value;
+};
 
 export function getNobt(identifier) {
-  return axios.get(apiBase + 'nobts/' + identifier);
+  return axios
+    .get(apiBase + 'nobts/' + identifier)
+    .then((log('api:get-nobt:successs')), log('api:get-nobt:error'));
 }
 
 export function createNobt(nobt) {
-  return axios.post(apiBase + "nobts", JSON.stringify(nobt), requestConfig);
+  return axios
+    .post(apiBase + "nobts", JSON.stringify(nobt), requestConfig)
+    .then(log('api:create-nobt:successs'), log('api:create-nobt:error'));
 }
