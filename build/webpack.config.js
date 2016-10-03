961675b124bf9cf46d872fd60000e54630465d22
Thomas Eizinger <thomas@nobt.io>
2016-10-03 15:56:24 +0200

    fix hack for importing global theme

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -6,11 +6,12 @@ import config from '../config'
 import _debug from 'debug'
 import read from 'read-file'
 
-const debug = _debug('app:webpack:config')
-const paths = config.utils_paths
-const {__DEV__, __PROD__, __TEST__} = config.globals
+const debug = _debug('app:webpack:config');
+const paths = config.utils_paths;
+const {__DEV__, __PROD__, __TEST__} = config.globals;
 
-debug('Create configuration.')
+debug('Create configuration.');
+
 const webpackConfig = {
   name: 'client',
   target: 'web',
@@ -20,20 +21,20 @@ const webpackConfig = {
     extensions: ['', '.js', '.jsx', '.json']
   },
   module: {}
-}
+};
 // ------------------------------------
 // Entry Points
 // ------------------------------------
 const APP_ENTRY_PATHS = [
   paths.client('main.js')
-]
+];
 
 webpackConfig.entry = {
   app: __DEV__
     ? APP_ENTRY_PATHS.concat(`webpack-hot-middleware/client?path=${config.compiler_public_path}__webpack_hmr`)
     : APP_ENTRY_PATHS,
   vendor: config.compiler_vendor
-}
+};
 
 // ------------------------------------
 // Bundle Output
@@ -42,7 +43,7 @@ webpackConfig.output = {
   filename: `[name].[${config.compiler_hash_type}].js`,
   path: paths.dist(),
   publicPath: config.compiler_public_path
-}
+};
 
 // ------------------------------------
 // Plugins
@@ -59,16 +60,16 @@ webpackConfig.plugins = [
       collapseWhitespace: true
     }
   })
-]
+];
 
 if (__DEV__) {
-  debug('Enable plugins for live development (HMR, NoErrors).')
+  debug('Enable plugins for live development (HMR, NoErrors).');
   webpackConfig.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   )
 } else if (__PROD__) {
-  debug('Enable plugins for production (OccurenceOrder, Dedupe & UglifyJS).')
+  debug('Enable plugins for production (OccurenceOrder, Dedupe & UglifyJS).');
   webpackConfig.plugins.push(
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
@@ -134,20 +135,20 @@ webpackConfig.module.loaders = [{
 {
   test: /\.json$/,
   loader: 'json'
-}]
+}];
 
 // ------------------------------------
 // Style Loaders
 // ------------------------------------
 // We use cssnano with the postcss loader, so we tell
 // css-loader not to duplicate minimization.
-const BASE_CSS_LOADER = 'css?sourceMap&-minimize'
+const BASE_CSS_LOADER = 'css?sourceMap&-minimize';
 
 // Add any packge names here whose styles need to be treated as CSS modules.
 // These paths will be combined into a single regex.
 const PATHS_TO_TREAT_AS_CSS_MODULES = [
     'react-toolbox'
-]
+];
 
 // If config has CSS modules enabled, treat this project's styles as CSS modules.
 if (config.compiler_css_modules) {
@@ -166,7 +167,7 @@ if (isUsingCSSModules) {
     'modules',
     'importLoaders=1',
     'localIdentName=[name]__[local]___[hash:base64:5]'
-  ].join('&')
+  ].join('&');
 
   webpackConfig.module.loaders.push({
     test: /\.scss$/,
@@ -177,7 +178,7 @@ if (isUsingCSSModules) {
       'postcss',
       'sass?sourceMap'
     ]
-  })
+  });
 
   webpackConfig.module.loaders.push({
     test: /\.css$/,
@@ -201,7 +202,8 @@ webpackConfig.module.loaders.push({
     'postcss',
     'sass?sourceMap'
   ]
-})
+});
+
 webpackConfig.module.loaders.push({
   test: /\.css$/,
   exclude: excludeCSSModules,
@@ -210,15 +212,19 @@ webpackConfig.module.loaders.push({
     BASE_CSS_LOADER,
     'postcss'
   ]
-})
+});
+
+var themeFile = paths.client("styles/theme.scss").replace(/\\/g, "\\\\");
+
+debug(`Importing theme.scss at ${themeFile}`);
 
 // ------------------------------------
 // Style Configuration
 // ------------------------------------
 webpackConfig.sassLoader = {
-  data: read.sync(paths.client('styles/theme.scss')),
+  data: `@import '${themeFile}';`,
   includePaths: paths.client('styles')
-}
+};
 
 webpackConfig.postcss = [
   cssnano({
@@ -236,7 +242,7 @@ webpackConfig.postcss = [
     safe: true,
     sourcemap: true
   })
-]
+];
 
 // File loaders
 /* eslint-disable */
@@ -248,7 +254,7 @@ webpackConfig.module.loaders.push(
   { test: /\.eot(\?.*)?$/,   loader: 'file?prefix=fonts/&name=[path][name].[ext]' },
   { test: /\.svg(\?.*)?$/,   loader: 'url?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml' },
   { test: /\.(png|jpg)$/,    loader: 'url?limit=8192' }
-)
+);
 /* eslint-enable */
 
 // ------------------------------------
@@ -258,14 +264,14 @@ webpackConfig.module.loaders.push(
 // need to use the extractTextPlugin to fix this issue:
 // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
 if (!__DEV__) {
-  debug('Apply ExtractTextPlugin to CSS loaders.')
+  debug('Apply ExtractTextPlugin to CSS loaders.');
   webpackConfig.module.loaders.filter((loader) =>
     loader.loaders && loader.loaders.find((name) => /css/.test(name.split('?')[0]))
   ).forEach((loader) => {
-    const [first, ...rest] = loader.loaders
+    const [first, ...rest] = loader.loaders;
     loader.loader = ExtractTextPlugin.extract(first, rest.join('!'))
     Reflect.deleteProperty(loader, 'loaders')
-  })
+  });
 
   webpackConfig.plugins.push(
     new ExtractTextPlugin('[name].[contenthash].css', {
