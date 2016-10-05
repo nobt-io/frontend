188a2eb78fd8275abaa5f0d98bc6a831ec060fa3
Thomas Eizinger <thomas@nobt.io>
2016-10-05 11:52:36 +0200

    move nobt module back to overview screen

--- src/routes/Nobt/routes/Overview/modules/Nobt.js
+++ src/routes/Nobt/routes/Overview/modules/Nobt.js
@@ -1,19 +1,50 @@
 import {getNobt} from 'api/api';
+import PersonDebtSummaryFactory from './PersonDebtSummaryFactory'
+import CurrencyFormatter from './CurrencyFormatter'
+import PersonSummaryFactory from './PersonSummaryFactory'
 
-import {actionHandlers as globalNobtActionHandlers, actionFactory as globalNobtActionFactory} from '../../../modules/NobtModule'
+import debug from 'debug';
 
 const actionNames = {
+  LOAD_NOBT: 'Nobt.LOAD_NOBT',
+  SET_NOBT: 'Nobt.SET_NOBT',
   CHANGE_TAB: 'Nobt.CHANGE_TAB',
   CHANGE_EXPENSES_VIEW_INFO: 'Nobt.CHANGE_EXPENSES_VIEW_INFO',
 };
 
 
-export const nobtActionFactory = {...globalNobtActionFactory,
+export const nobtActionFactory = {
+  loadNobt: (id) => {
+    return (dispatch, getState) => {
+      getNobt(id).then(response => {
+        dispatch({type: actionNames.SET_NOBT, payload: {nobt: response.data}});
+      }, error => {
+        //TODO: error handling
+      });
+    }
+  },
   changeTab: (tabName) => ({type: actionNames.CHANGE_TAB, payload: {tabName: tabName}}),
   changeExpenseViewInfo: (filter, sort) => ({type: actionNames.CHANGE_EXPENSES_VIEW_INFO, payload: {filter, sort}}),
 };
 
-const actionHandlers = {...globalNobtActionHandlers,
+const actionHandlers = {
+  [actionNames.SET_NOBT]: (state, action) => {
+
+    //ioc for the poor
+    const currencyFormatter = new CurrencyFormatter("EUR");
+    const personSummaryFactory = new PersonSummaryFactory(currencyFormatter);
+    const transactionFactory = new PersonDebtSummaryFactory(action.payload.nobt.transactions, personSummaryFactory);
+
+    const total = currencyFormatter.getCurrencyAmount(getTotal(action.payload.nobt.expenses));
+    const name = action.payload.nobt.name;
+    const members = action.payload.nobt.participatingPersons;
+    const expenses = action.payload.nobt.expenses.map(e => getExpense(e, personSummaryFactory));
+    const expensesFiltered = expenses;
+
+    var transactions = members.map(m => transactionFactory.computeSummaryForPerson(m));
+
+    return {...state, name, total, members, transactions, expenses, expensesFiltered};
+  },
   [actionNames.CHANGE_TAB]: (state, action) => {
 
     var tabNameIndexMapping = {
@@ -53,6 +84,22 @@ const actionHandlers = {...globalNobtActionHandlers,
     return {...state, expensesFiltered,  expensesViewInfo: {...state.expensesViewInfo, filter, sort}};
   }
 };
+
+const getTotal = (expenses) => {
+  return expenses.reduce((total, expense) => total + expense.shares.reduce((expenseTotal, share) => expenseTotal + share.amount, 0), 0);
+};
+
+const getExpense = (expense, personSummaryFactory) => {
+  const total = expense.shares.reduce((expenseTotal, share) => expenseTotal + share.amount, 0);
+
+  const name = expense.name;
+  const strategy = expense.splitStrategy;
+  const debtee = personSummaryFactory.cratePersonSummary({name: expense.debtee, amount: total});
+  const debtors = expense.shares.map(s => personSummaryFactory.createExpensePerson(s));
+
+  return {name, strategy, debtee, debtors}
+};
+
 
 export const initialState = {
   total: 0,
