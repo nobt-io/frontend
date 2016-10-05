88cda8668bb120943457e09dea5e11f2068b8ac7
duffleit <davidleitner@live.at>
2016-10-05 14:09:11 +0200

    added defaultProps to Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,21 +4,27 @@ import Modal from "react-responsive-modal";
 
 export const LowerScreenModal = (props) => {
 
-  const onClose = props.onClose || (() => {
-    });
-  const active = props.active || false;
-
-  const header = props.header || '';
-  const headerObj = header == '' ? (<div></div>) : (<div className={styles.header}>{header}</div> );
+  const headerIsEmpty = header == '';
 
   return (
-    <Modal overlayClassName={styles.overlay} modalClassName={styles.modal} open={active} onClose={onClose}>
+    <Modal overlayClassName={styles.overlay} modalClassName={styles.modal} open={props.active} onClose={props.onClose}>
       <div className={styles.content}>
-        {headerObj}
+        <div className={headerIsEmpty ? "" : styles.header}>{props.header}</div>
         {props.children}
       </div>
     </Modal>
   );
+};
+
+LowerScreenModal.propTypes = {
+  onClose: React.PropTypes.func.isRequired,
+  active: React.PropTypes.bool
+};
+
+LowerScreenModal.defaultProps = {
+  onClose: () => { },
+  active: false,
+  header: ''
 };
 
 export default LowerScreenModal;
