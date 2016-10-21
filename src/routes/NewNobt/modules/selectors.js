035433a2a6b836a046dd73b192090f7654e92c78
Thomas Eizinger <thomas.eizinger@gmail.com>
2016-10-21 17:11:05 +0200

    Log input and ouput of all selectors.

--- src/routes/NewNobt/modules/selectors.js
+++ src/routes/NewNobt/modules/selectors.js
@@ -1,4 +1,14 @@
-import { createSelector } from "reselect";
+import { createSelectorWithChangeCallback } from 'reselect-change-memoize';
+
+import _debug from "debug";
+
+function createSelector(loggerName, ...args) {
+  return createSelectorWithChangeCallback((lastArgs, lastResult, newArgs, newResult) => {
+
+    _debug(`selector:${loggerName}`)(`(${newArgs}) => ${newResult}`)
+
+  }, ...args);
+}
 
 export const getSelectedCurrency = (state) => state.NewNobt.selectedCurrency;
 export const getChosenName = (state) => state.NewNobt.chosenName;
@@ -8,11 +18,12 @@ export const getCreationStatus = (state) => state.NewNobt.createNobtStatus;
 export const getCreatedNobtId = (state) => state.NewNobt.createdNobtId;
 
 
-export const isNameValid = createSelector([ getChosenName ], name => name !== undefined && name.length > 0);
-export const arePersonNamesValid = createSelector([ getPersonNames ], names => names !== undefined && names.length > 0);
-export const canCreateNobt = createSelector([ isNameValid, arePersonNamesValid ], (isNameValid, arePersonsValid) => isNameValid && arePersonsValid);
+export const isNameValid = createSelector("isNameValid", [ getChosenName ], name => name !== undefined && name.length > 0);
+export const arePersonNamesValid = createSelector("arePersonNamesValid", [ getPersonNames ], names => names !== undefined && names.length > 0);
+export const  canCreateNobt = createSelector("canCreateNobt", [ isNameValid, arePersonNamesValid ], (isNameValid, arePersonsValid) => isNameValid && arePersonsValid);
 
 // different from the other selectors, as it returns a function and not an object
-export const isEvilTwinFactory = createSelector([ getPersonNames ], names => {
+export const isEvilTwinFactory = createSelector("isEvilTwinFactory", [ getPersonNames ], names => {
   return (candidate) => names.indexOf(candidate) > -1;
 });
+
