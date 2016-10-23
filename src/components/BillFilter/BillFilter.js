3d4f032436f178813bc7bdd3da2164ac0548059d
Thomas Eizinger <thomas.eizinger@gmail.com>
2016-10-23 19:01:24 +0200

    Rewrite BillFilter with new Overlay.

--- src/components/BillFilter/BillFilter.js
+++ src/components/BillFilter/BillFilter.js
@@ -3,36 +3,45 @@ import styles from "./BillFilter.scss";
 
 import Header from "components/Header"
 import FontIcon from "react-toolbox/lib/font_icon";
-import {IconButton} from 'react-toolbox/lib/button';
+import { IconButton } from 'react-toolbox/lib/button';
 
-import PersonSelectorOverlay from "components/PersonSelectorOverlay";
+import CloseButton from "components/CloseButton"
+import PersonPicker from "components/PersonPicker"
+
 import { List, ListItem } from "react-toolbox/lib/list";
 
 import Overlay from "components/Overlay/Overlay"
+import { Avatar } from "components/Avatar";
 
-import {Avatar} from "components/Avatar";
+import Visibility from "const/Visibility"
 
 export const BillFilter = React.createClass({
 
-  onFilterModalClose: function () {
-    this.setState({sortModalIsActive: false, filterModalIsActive: false});
+  getInitialState() {
+    return {
+      sortPropertyPickerOverlayVisibility: Visibility.HIDDEN,
+      personFilterOverlayVisibility: Visibility.HIDDEN,
+    }
   },
 
-  onFilterModalOpen: function () {
-    this.setState({sortModalIsActive: false, filterModalIsActive: true});
+  closePersonFilterOverlay() {
+    this.setState({ personFilterOverlayVisibility: Visibility.HIDDEN })
   },
 
-  onSortModalClose: function () {
-    this.setState({sortModalIsActive: false, filterModalIsActive: false});
+  openPersonFilterOverlay() {
+    this.setState({ personFilterOverlayVisibility: Visibility.VISIBLE })
   },
 
-  onSortModalOpen: function () {
-    this.setState({sortModalIsActive: true, filterModalIsActive: false});
+  closeSortPropertyPickerOverlay() {
+    this.setState({ sortPropertyPickerOverlayVisibility: Visibility.HIDDEN })
+  },
+
+  openSortPropertyPickerOverlay() {
+    this.setState({ sortPropertyPickerOverlayVisibility: Visibility.VISIBLE })
   },
 
   render: function () {
 
-    const {filterModalIsActive, sortModalIsActive} = this.state || {filterModalIsActive: false, sortModalIsActive: false};
     const {personNames, onFilterChange, onSortChange, currentFilter, currentSort, onReset} = this.props;
 
     const defaultFilter = currentSort == "Date" && currentFilter == "";
@@ -41,38 +50,40 @@ export const BillFilter = React.createClass({
       ? (<b>All</b>)
       : (<span className={styles.personFilter}>
           <b>{currentFilter}</b>
-          <Avatar name={currentFilter} size={20} fontSize={11}/>
+          <Avatar name={currentFilter} size={20} fontSize={11} />
         </span>);
 
     return (
       <div className={styles.container}>
-        <PersonSelectorOverlay
-          title={"Filter bills by"}
-          active={filterModalIsActive} onFilterChange={onFilterChange} onClose={this.onFilterModalClose}
-          names={personNames} canSelectAll={true}/>
 
-        <Overlay ref={ (overlayRef) => this._sortPropertyPickerOverlay = overlayRef }>
-          <div className={styles.sortPropertyListContainer}>
-            <Header
-              left={<h3>Sort by</h3>}
-              right={<IconButton icon="close" onClick={() => this._sortPropertyPickerOverlay.close()} />}
-              theme={ {
+        <Overlay visibility={this.state.personFilterOverlayVisibility} onClickOutside={this.closePersonFilterOverlay}>
+          <Header
+            left={<h3>Filter bills by</h3>}
+            right={<CloseButton onClick={this.closePersonFilterOverlay}/>}
+          />
+          <PersonPicker names={personNames} onPersonPicked={ (name) => {
+            // this.setMetaData({name});
+            this.closePersonFilterOverlay()
+          }}/>
+        </Overlay>
 
-              } }
-            />
-            <List selectable ripple>
-              <ListItem key="Sort.ByDate" onClick={onSortChange} caption={"Date"} leftIcon="access_time"/>
-              <ListItem key="Sort.ByAmount" onClick={onSortChange} caption={"Amount"} leftIcon="timeline"/>
-            </List>
-          </div>
+        <Overlay visibility={this.state.sortPropertyPickerOverlayVisibility} onClickOutside={this.closeSortPropertyPickerOverlay}>
+          <Header
+            left={<h3>Sort by</h3>}
+            right={<IconButton icon="close" onClick={this.closeSortPropertyPickerOverlay} />}
+          />
+          <List selectable ripple>
+            <ListItem key="Sort.ByDate" onClick={onSortChange} caption={"Date"} leftIcon="access_time" />
+            <ListItem key="Sort.ByAmount" onClick={onSortChange} caption={"Amount"} leftIcon="timeline" />
+          </List>
         </Overlay>
 
         <div>
           <span onClick={onReset} style={{display: defaultFilter ? "none" : "inline"}}
-                className={styles.filterIcon}><FontIcon value='clear'/></span>
+                className={styles.filterIcon}><FontIcon value='clear' /></span>
 
-          <span onClick={() => this._sortPropertyPickerOverlay.open()} className={styles.filter}>sort by <b>{currentSort}</b></span>
-          <span onClick={this.onFilterModalOpen} className={styles.filter}>filter by {filter}</span>
+          <span onClick={this.openSortPropertyPickerOverlay} className={styles.filter}>sort by <b>{currentSort}</b></span>
+          <span onClick={this.openPersonFilterOverlay} className={styles.filter}>filter by {filter}</span>
         </div>
 
       </div>);
