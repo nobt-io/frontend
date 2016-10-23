2c43d1e5e6871fba6bdbedeee1f8d96150067da6
Thomas Eizinger <thomas.eizinger@gmail.com>
2016-10-24 00:18:57 +0200

    Always render everything because renderer null works.
    Not rendering at least both containers all the time breaks the layout as the right container is suddenly the left one if the left one does not exist in the first place.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,15 @@
 import React from "react";
-import AppBar from "react-toolbox/lib/app_bar";
 import styles from "./Header.scss";
 
 export const Header = (props) => (
   <div className={styles.headerContainer}>
     <div className={`${props.theme.header} ${styles.header}`}>
-      {
-        (props.left) &&
-        (<div className={`${props.theme.leftContainer} ${styles.leftContainer}`}>
-          {props.left}
-        </div>)
-      }
-      {
-        (props.right) &&
-        (<div className={`${props.theme.rightContainer} ${styles.rightContainer}`}>
-          {props.right}
-        </div>)
-      }
+      <div className={`${props.theme.leftContainer} ${styles.leftContainer}`}>
+        {props.left}
+      </div>
+      <div className={`${props.theme.rightContainer} ${styles.rightContainer}`}>
+        {props.right}
+      </div>
     </div>
   </div>
 );
