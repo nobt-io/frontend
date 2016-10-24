56ac20fcc21e9b6e736e1821c9f2007f0d1b3539
Thomas Eizinger <thomas@nobt.io>
2016-10-24 22:42:33 +0200

    Tag every deploy with the current revision number.
    Note that we use the env  variable provided by GitLab CI as we only want to happen releases through GitLab anyway. We don't care and also don't want to track about local "releases" because these should not happen anyway.

--- config/index.js
+++ config/index.js
@@ -79,6 +79,7 @@ config.globals = {
   'process.env'  : {
     'NODE_ENV' : JSON.stringify(config.env)
   },
+  'COMMIT_HASH'  : process.env.CI_BUILD_REF,
   'NODE_ENV'     : config.env,
   '__DEV__'      : config.env === 'development',
   '__PROD__'     : config.env === 'production',
