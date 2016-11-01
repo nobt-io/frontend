517c492ff8db2e422a633fd9579478f94484ca00
Thomas Eizinger <thomas@nobt.io>
2016-11-01 21:46:12 +0100

    Render a button that encourages the user to select the debtee.
    We also apply the same box-shadow that is used for the button after the user selected a debtee so the UI is consistent and the user should know, that he can also click on the element that is displayed once he selected a debtee.

--- src/routes/Nobt/components/AddBillForm/DebteePicker/DebteePicker.js
+++ src/routes/Nobt/components/AddBillForm/DebteePicker/DebteePicker.js
@@ -1,13 +1,11 @@
-import React from 'react'
-
-import Overlay from "components/Overlay"
-import PersonPicker from "components/PersonPicker"
-import { Avatar, AvatarSize } from "components/Avatar"
-import Header from "components/Header"
-
-import Visibility from "const/Visibility"
-
-import styles from './DebteePicker.scss'
+import React from "react";
+import Button from "react-toolbox/lib/button";
+import Overlay from "components/Overlay";
+import PersonPicker from "components/PersonPicker";
+import { Avatar, AvatarSize } from "components/Avatar";
+import Header from "components/Header";
+import Visibility from "const/Visibility";
+import styles from "./DebteePicker.scss";
 
 const DebteePicker = React.createClass({
 
@@ -23,40 +21,55 @@ const DebteePicker = React.createClass({
   },
 
   closeOverlay() {
-    this.setState({ overlayVisibility: Visibility.HIDDEN })
+    this.setState({overlayVisibility: Visibility.HIDDEN})
   },
 
   openOverlay() {
-    this.setState({ overlayVisibility: Visibility.VISIBLE })
+    this.setState({overlayVisibility: Visibility.VISIBLE})
+  },
+
+  renderDebteePicker() {
+    return (
+      <div className={styles.DebteePicker} onClick={this.openOverlay}>
+        <div className={styles.avatar}>
+          <Avatar name={this.props.value} size={AvatarSize.BIG} />
+        </div>
+
+        <div className={styles.debteeContainer}>
+          <div className={styles.label}>paid by</div>
+          <div className={styles.debtee}>{this.props.value}</div>
+        </div>
+      </div>
+    )
+  },
+
+  renderEmptyContainer() {
+    return (
+      <Button raised onClick={this.openOverlay}>Who paid?</Button>
+    )
   },
 
   render() {
     return (
-      <div className={this.props.className}>
-        <div className={styles.DebteePicker} onClick={this.openOverlay}>
-          <div className={styles.avatar}>
-            <Avatar name={this.props.value} size={AvatarSize.BIG}/>
-          </div>
 
-          <div className={styles.debteeContainer}>
-            <div className={styles.label}>paid by</div>
-            <div className={styles.debtee}>{this.props.value}</div>
-          </div>
-        </div>
+      <div className={this.props.className}>
+
+        {this.props.value === null && this.renderEmptyContainer()}
+        {this.props.value !== null && this.renderDebteePicker()}
 
         <Overlay visibility={this.state.overlayVisibility} onClickOutside={this.closeOverlay}>
-          <Header left={<h3>Who paid?</h3>}/>
-          <PersonPicker names={this.props.names} onPersonPicked={this.handleOnPersonPicked}/>
+          <Header left={<h3>Who paid?</h3>} />
+          <PersonPicker names={this.props.names} onPersonPicked={this.handleOnPersonPicked} />
         </Overlay>
       </div>
-    )
+    );
   }
 });
 
 export default DebteePicker;
 
 DebteePicker.propTypes = {
-  value: React.PropTypes.string.isRequired,
-  names: React.PropTypes.arrayOf( React.PropTypes.string ).isRequired,
+  value: React.PropTypes.string,
+  names: React.PropTypes.arrayOf(React.PropTypes.string).isRequired,
   onDebteePicked: React.PropTypes.func.isRequired
 };
