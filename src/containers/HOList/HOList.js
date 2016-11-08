ee7563360105508561ef3a988fc25eb819319bc4
Thomas Eizinger <thomas@nobt.io>
2016-11-08 02:10:54 +0100

    Merge actual children of HOList with rendered items.

--- src/containers/HOList/HOList.js
+++ src/containers/HOList/HOList.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import {List} from "react-toolbox/lib/list"
+import React from "react";
+import { List } from "react-toolbox/lib/list";
+
 export const HOList = (props) => (
   <List {...props}>
-    {props.items.filter(props.shouldRenderItem).map(props.renderItem)}
+    {createActualItems(props)}
   </List>
 );
 
 export default HOList
+
+const createActualItems = (props) => {
+
+  var filteredItems = props.items.filter(props.shouldRenderItem).map(props.renderItem);
+
+  if (props.children) {
+    filteredItems.push(props.children);
+  }
+
+  return filteredItems;
+}
 
 HOList.propTypes = {
   items: React.PropTypes.array.isRequired,
