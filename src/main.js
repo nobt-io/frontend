0194061179a9290394417f1864330ce2cbba9216
Thomas Eizinger <thomas@nobt.io>
2017-01-21 17:13:13 +0100

    Disable error rendering so we can observe the error in the console log.

--- src/main.js
+++ src/main.js
@@ -65,23 +65,26 @@ let render = () => {
 // This code is excluded from production bundle
 if (__DEV__) {
   if (module.hot) {
-    // Development render functions
-    const renderApp = render
-    const renderError = (error) => {
-      const RedBox = require('redbox-react').default
 
-      ReactDOM.render(<RedBox error={error}/>, MOUNT_NODE)
-    }
+    /*
+     // Development render functions
+     const renderApp = render
+     const renderError = (error) => {
+     const RedBox = require('redbox-react').default
 
-    // Wrap render in try/catch
-    render = () => {
-      try {
-        renderApp()
-      } catch (error) {
-        renderError(error)
-      }
-    }
+     ReactDOM.render(<RedBox error={error}/>, MOUNT_NODE)
+     }
 
+     // Wrap render in try/catch
+     render = () => {
+     try {
+     renderApp()
+     } catch (error) {
+     renderError(error)
+     }
+     }
+
+     */
     // Setup hot module replacement
     module.hot.accept('./routes/index', () => {
       setTimeout(() => {
