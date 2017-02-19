c04c23fb6e50ef308661d303f4f2bf590cc7ea58
Thomas Eizinger <thomas@nobt.io>
2017-02-19 18:58:41 +0100

    Change to card based bill list layout and add menu for each bill

--- src/routes/App/components/BillItem/BillItem.js
+++ src/routes/App/components/BillItem/BillItem.js
@@ -1,11 +1,14 @@
 import React from "react";
-import styles from "./BillItem.scss";
-import { AvatarList, AvatarSize } from "components/Avatar";
-import Amount from "components/Amount";
-import { Person, AvatarPosition } from "components/Person";
 import { connect } from "react-redux";
 import withNavigation from "../../../../components/hoc/withNavigation";
+import { Card, CardTitle, CardText } from "react-toolbox/lib/card";
+import { FormattedMessage } from "react-intl";
 import LocationBuilder from "../../modules/navigation/LocationBuilder";
+import BillCardTitleTheme from "./BillCardTitleTheme.scss";
+import OverflowMenuTheme from "./OverflowMenuTheme.scss";
+import styles from "./BillItem.scss";
+import Amount from "components/Amount";
+import { IconMenu, MenuItem } from "react-toolbox/lib/menu";
 
 class BillItem extends React.Component {
 
@@ -19,26 +22,41 @@ class BillItem extends React.Component {
     const debtee = bill.debtee;
 
     return (
-      <div onClick={ () => LocationBuilder.fromWindow().push(bill.id).apply(this.props.push) } className={styles.billContainer}>
+      <div className={styles.billItemContainer}>
+        <Card onClick={ () => LocationBuilder.fromWindow().push(bill.id).apply(this.props.push) }>
+          <CardTitle
+            theme={BillCardTitleTheme}
+            title={bill.name}
+            subtitle={<FormattedMessage
+              id="BillItem.subtitle"
+              defaultMessage="{amount} paid by {debtee}"
+              values={{
+                amount: <Amount value={bill.debtee.amount} />,
+                debtee: bill.debtee.name
+              }} />}>
+          </CardTitle>
+          <CardText>
+            <FormattedMessage
+              id="BillItem.involvedPeople"
+              defaultMessage="{numberOfPeople, plural,
+    =1 {{first}}
+    =2 {{first} and {second}}
+    =3 {{first}, {second} and {third}}
+    other {{first}, {second} and {remaining} others}
+}."
+              values={{
+                numberOfPeople: bill.debtors.length,
+                first: bill.debtors[ 0 ].name,
+                second: bill.debtors[ 1 ].name,
+                third: bill.debtors[ 2 ].name,
+                remaining: bill.debtors.length - 2
+              }} />
+          </CardText>
+        </Card>
 
-        <div className={styles.billMetaDataContainer}>
-          <div className={styles.nameContainer}>
-            <span>{bill.name}</span>
-          </div>
-          <div className={styles.amountContainer}>
-            <Amount value={debtee.amount} />
-          </div>
-        </div>
-
-        <div className={styles.billPersonInformationContainer}>
-          <div className={styles.debtee}>
-            <Person name={debtee.name} avatarPosition={AvatarPosition.LEFT} avatarSize={AvatarSize.BIG} />
-          </div>
-          <div className={styles.debtors}>
-            <AvatarList names={bill.debtors.map(debtor => debtor.name)} size={AvatarSize.MEDIUM} />
-          </div>
-        </div>
-
+        <IconMenu theme={OverflowMenuTheme}>
+          <MenuItem icon="delete" caption="Delete bill" disabled/>
+        </IconMenu>
       </div>
     );
   }
