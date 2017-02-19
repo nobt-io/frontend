898da91d8e873ee1a866e00e6353ac9a688a0ae8
Thomas Eizinger <thomas@nobt.io>
2017-02-19 18:44:58 +0100

    Harmonize styles of ListItems that display person information

--- src/routes/App/routes/filter/components/FilterBillOverlay.js
+++ src/routes/App/routes/filter/components/FilterBillOverlay.js
@@ -6,10 +6,10 @@ import Dialog from "components/Dialog";
 import { getMembers } from "../../../modules/currentNobt/selectors";
 import HOList from "containers/HOList";
 import { ListItem, ListDivider } from "react-toolbox/lib/list";
-import { Person, AvatarPosition } from "components/Person";
-import { AvatarSize } from "components/Avatar";
+import { Avatar } from "components/Avatar";
 import { getBillFilter } from "../../../modules/viewState/selectors";
 import listItemTheme from "./ListItemTheme.scss";
+import listTheme from "./ListTheme.scss";
 import DialogTheme from "components/Dialog/DialogTheme.scss";
 
 const FilterBillOverlay = (props) => (
@@ -17,29 +17,32 @@ const FilterBillOverlay = (props) => (
   <Dialog>
     <h3 className={DialogTheme.header}>Filter bills</h3>
     <HOList
+      theme={listTheme}
       selectable
       items={props.members}
       renderItem={ (name) => (
-        <ListItem rightIcon={ name === props.currentFilter ? "check_circle" : "radio_button_unchecked" }
-                  theme={listItemTheme}
-                  key={name}
-                  onClick={ () => {
-                    props.updateBillFilter(name)
-                    props.goBack()
-                  } }>
-          <Person avatarSize={AvatarSize.MEDIUM} avatarPosition={AvatarPosition.LEFT} name={name} />
-        </ListItem>
+        <ListItem
+          rightIcon={ name === props.currentFilter ? "check_circle" : "radio_button_unchecked" }
+          theme={listItemTheme}
+          key={name}
+          onClick={ () => {
+            props.updateBillFilter(name)
+            props.goBack()
+          } }
+          leftIcon={<Avatar name={name} medium />}
+          caption={name}
+        />
       )}>
       <ListDivider />
-      <ListItem rightIcon={ "" === props.currentFilter ? "check_circle" : "radio_button_unchecked"  }
-                theme={listItemTheme}
-                key={"None"}
-                onClick={ () => {
-                  props.updateBillFilter("")
-                  props.goBack()
-                } }>
-        <p className={listItemTheme.clear}>None</p>
-      </ListItem>
+      <ListItem
+        rightIcon={ "" === props.currentFilter ? "check_circle" : "radio_button_unchecked"  }
+        theme={listItemTheme}
+        key={"None"}
+        caption="None"
+        onClick={ () => {
+          props.updateBillFilter("")
+          props.goBack()
+        } } />
     </HOList>
   </Dialog>
 );
