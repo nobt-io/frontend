a3cd4d162fee74b87aecd4adad9f85df14fcba45
Thomas Eizinger <thomas@nobt.io>
2017-05-17 01:19:35 +0200

    Remove now failing tests for api resolution

--- __tests__/axiosFactory.test.js
+++ __tests__/axiosFactory.test.js
@@ -6,29 +6,4 @@ describe("API-Factory", () => {
 
     expect(instance.defaults.baseURL).toBe("http://localhost:8080");
   });
-
-  it("should resolve API-Host for dev host", () => {
-    var instance = factory("nobt-io-frontend-dev.cfapps.io");
-
-    expect(instance.defaults.baseURL).toBe("http://nobt-io-dev.cfapps.io");
-  });
-
-  it("should resolve API-Host for beta host", () => {
-    var instance = factory("nobt-io-frontend-beta.cfapps.io");
-
-    expect(instance.defaults.baseURL).toBe("http://nobt-io-beta.cfapps.io");
-  });
-
-
-  it("should resolve API-Host for production host", () => {
-    var instance = factory("nobt-io-frontend.cfapps.io");
-
-    expect(instance.defaults.baseURL).toBe("http://nobt-io.cfapps.io");
-  });
-
-  it("should resolve API-Host for review instances", () => {
-    var instance = factory("review-jest-support.cfapps.io");
-
-    expect(instance.defaults.baseURL).toBe("http://nobt-io-dev.cfapps.io");
-  });
 });
