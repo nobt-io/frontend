588e63d574ec83e98b3a3effca1606e2e459b0b5
david <davidleitner@live.at>
2017-05-23 17:25:58 +0200

    Set Link of StartButton to Create Route

--- src/routes/Landing/components/LandingPage/StartButton/StartButton.js
+++ src/routes/Landing/components/LandingPage/StartButton/StartButton.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./StartButton.scss";
+import LocationBuilder from "../../../../App/modules/navigation/LocationBuilder";
 
 const StartButton = (props) => {
 
   let className = (props.active) ? styles.activeButton : styles.button;
 
   return (
-    <a className={className} href="#">
+    <a className={className} href="create">
       Start splitting your bills
     </a>
   )
