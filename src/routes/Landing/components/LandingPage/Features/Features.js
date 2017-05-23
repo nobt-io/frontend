3049a10cbfc580e727c4b678178c9ea8348b11b8
duffleit <davidleitner@live.at>
2017-05-23 19:22:46 +0200

    Adding Texts for Features

--- src/routes/Landing/components/LandingPage/Features/Features.js
+++ src/routes/Landing/components/LandingPage/Features/Features.js
@@ -14,24 +14,21 @@ export default class Features extends React.Component {
             <div className={styles.feature}>
               <div className={styles.icon}><i className="fa fa-dashboard"></i></div>
               <h3>Easy to Use</h3>
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-                erat, sed diam voluptua. </p>
+              <p>Nobt.io works without registration. Start by creating a nobt and share the link with your friends.</p>
             </div>
           </Col>
           <Col sm={4}>
             <div className={styles.feature}>
               <div className={styles.icon}><i className="fa fa-cloud"></i></div>
               <h3>Available Everywhere</h3>
-              <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et doloremagna aliquyam
-                erat, sed diam voluptua. </p>
+              <p>Nobts are stored in the cloud, so you can access them from any device, anytime, no matter where you are.</p>
             </div>
           </Col>
           <Col sm={4}>
             <div className={styles.feature}>
               <div className={styles.icon}><i className="fa fa-github"></i></div>
               <h3>Open Source</h3>
-              <p >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-                erat, sed diam voluptua. </p>
+              <p>We believe that the Web should be open that's why we share everything about nobt.io, except your data.</p>
             </div>
           </Col>
         </Row>
