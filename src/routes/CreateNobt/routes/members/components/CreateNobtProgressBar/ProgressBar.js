c4d9fa3161eda3b7af21b99a95ae8f9a95b4ffb4
Thomas Eizinger <thomas@nobt.io>
2017-08-21 01:01:31 +0200

    Replace Button with ProgressBar during HTTP-request
    During the asynchronous action of creating the nobt, the button is
    replaced with a progress bar for two reasons:
    
    - To show the user that something is happening
    - To prevent the user from clicking it again
    
    Upon failure, the usual snackbar appears that asks the user whether to
    try it again. The progress bar also works if the user uses the snackbar
    to kickoff the creation.

---
+++ src/routes/CreateNobt/routes/members/components/CreateNobtProgressBar/ProgressBar.js
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { ProgressBar } from "react-toolbox/lib/progress_bar/index";
+import ProgressBarTheme from "./ProgressBarTheme.scss";
+
+export default (props) => <ProgressBar theme={ProgressBarTheme} type="circular" mode="indeterminate" />
