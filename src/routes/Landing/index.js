447138bd1ea04839ef6c58ce8d3a0ea2ab7c8a12
Thomas Eizinger <thomas@nobt.io>
2017-08-23 15:38:39 +0200

    Undo unnecessary change

--- src/routes/Landing/index.js
+++ src/routes/Landing/index.js
@@ -1,9 +1,9 @@
+import LandingPage from "./components/LandingPage";
 import withNavigation from "../../components/hoc/withNavigation";
-import LandingPage from "./components/LandingPage"
 
 // Sync route definition
 export default (store) => {
   return {
-    component: withNavigation(LandingPage)
+    component : withNavigation(LandingPage)
   }
 }
