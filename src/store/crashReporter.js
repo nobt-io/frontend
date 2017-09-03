cf1b4c1185f401ef0efd403ba5bc8b82d5e09f57
Thomas Eizinger <thomas@nobt.io>
2017-09-04 01:02:09 +0200

    Simplify crash-reporting redux middleware
    1. We don't need to wrap the breadcrumb call in try-catch
    2. Capturing a message would trigger the reporting dialog, which we
    don't want in this case. Monitoring of errors concerning
    server communication should happen on the server anyway.
    3. With the reduced code in the try-catch block, we can now make use of
    Raven.context and simply pass in the extra data.

--- src/store/crashReporter.js
+++ src/store/crashReporter.js
@@ -1,38 +1,23 @@
-import AsyncActionStatus from "../const/AsyncActionStatus";
-
 export const crashReporter = store => next => action => {
 
   if (!Raven || !Raven.isSetup()) {
     return next(action);
   }
 
-  try {
+  const {type, ...other} = {...action};
 
-    const {type, ...other} = {...action};
-
-    Raven.captureBreadcrumb({
-      category: "redux",
-      message: type,
-      data: {
-        ...other
-      }
-    });
-
-    if (other.payload && other.payload.status === AsyncActionStatus.FAILED) {
-      Raven.captureMessage("Failed async action")
+  Raven.captureBreadcrumb({
+    category: "redux",
+    message: type,
+    data: {
+      ...other
     }
+  });
 
-    return next(action); // dispatch
-  } catch (err) {
-
-    Raven.captureException(err, { // send to crash reporting tool
-      extra: {
-        action,
-        state: store.getState() // dump application state
-      }
-    });
-    Raven.showReportDialog();
-
-    throw err; // re-throw error
-  }
+  Raven.context({
+    extra: {
+      action,
+      state: store.getState() // dump application state
+    }
+  }, () => next(action));
 };
