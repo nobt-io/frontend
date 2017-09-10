1799c6f620c45c2ab4e9ec32e90b8a092e36edca
Thomas Eizinger <thomas@nobt.io>
2017-09-10 20:06:38 +0200

    Add documentation

--- src/styles/custom-component-themes/index.js
+++ src/styles/custom-component-themes/index.js
@@ -1,5 +1,6 @@
 /**
- * https://github.com/react-toolbox/react-toolbox/tree/master#using-sass-imports-and-context
+ * This file specifies all the custom themes that we need in our application.
+ * For more information on how to use it see: https://github.com/react-toolbox/react-toolbox/tree/master#using-sass-imports-and-context
  */
 
 import AppBarTheme from "./appBarTheme.scss"
