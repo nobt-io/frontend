b78fd6775fec4ad9233a62acb002a9d89a673df1
Thomas Eizinger <thomas@nobt.io>
2017-09-14 21:51:53 +0200

    Initial css configuration for FAB menu

--- src/routes/App/components/AddBillFAB/AddBillFAB.js
+++ src/routes/App/components/AddBillFAB/AddBillFAB.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./AddBillFAB.scss";
 import { Button } from "react-toolbox/lib/button";
 import withNavigation from "components/hoc/withNavigation";
-import classNames from "classNames";
+import classNames from "classnames";
 import LocationBuilder from "../../modules/navigation/LocationBuilder";
 
 // TODO: Use a real FAB library here that allows to hide the button on scroll
@@ -19,15 +19,43 @@ class AddBillFAB extends React.Component {
   }
 
   render = () => (
-    <Button
-      icon='add'
-      className={classNames(styles.button, {
+    <div className={styles.fab}>
+
+      <div className={classNames(styles.fabItems, {
         [styles.expanded]: this.state.expanded
-      })}
-      primary
-      floating
-      onClick={ this.toggleExpanded }
-    />
+      })}>
+        <Button
+          className={classNames(styles.fabItem, {
+            [styles.expanded]: this.state.expanded
+          })}
+          icon='add'
+          primary
+          floating
+          mini
+          onClick={ this.toggleExpanded }
+        />
+        <Button
+          className={classNames(styles.fabItem, {
+            [styles.expanded]: this.state.expanded
+          })}
+          icon='add'
+          primary
+          floating
+          mini
+          onClick={ this.toggleExpanded }
+        />
+      </div>
+
+      <Button
+        icon='add'
+        className={classNames(styles.button, {
+          [styles.expanded]: this.state.expanded
+        })}
+        primary
+        floating
+        onClick={ this.toggleExpanded }
+      />
+    </div>
   )
 }
 
