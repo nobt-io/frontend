61cd2e6ccbc933e4e259dada5c4510adbf0580d3
Thomas Eizinger <thomas@nobt.io>
2017-09-14 17:07:59 +0200

    Rotate button on click

--- src/routes/App/components/AddBillFAB/AddBillFAB.js
+++ src/routes/App/components/AddBillFAB/AddBillFAB.js
@@ -2,21 +2,33 @@ import React from "react";
 import styles from "./AddBillFAB.scss";
 import { Button } from "react-toolbox/lib/button";
 import withNavigation from "components/hoc/withNavigation";
+import classNames from "classNames";
 import LocationBuilder from "../../modules/navigation/LocationBuilder";
 
 // TODO: Use a real FAB library here that allows to hide the button on scroll
-export const AddBillFAB = (props) => {
+class AddBillFAB extends React.Component {
 
-  return (
+  state = {
+    expanded: false
+  };
+
+  toggleExpanded = () => {
+    this.setState({
+      expanded: !this.state.expanded
+    })
+  }
+
+  render = () => (
     <Button
       icon='add'
-      className={styles.button}
+      className={classNames(styles.button, {
+        [styles.expanded]: this.state.expanded
+      })}
       primary
       floating
-      onClick={ () => LocationBuilder.fromWindow().push("newBill").apply(props.push) }
+      onClick={ this.toggleExpanded }
     />
   )
-};
-
+}
 
 export default withNavigation(AddBillFAB)
