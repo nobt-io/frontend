c78246cde327e8c97cd11a7306e8ca8a80cb800e
Thomas Eizinger <thomas@nobt.io>
2017-12-17 23:39:56 +0100

    Remove HeadRoom component
    Fixes #93

--- src/routes/App/components/Nobt.js
+++ src/routes/App/components/Nobt.js
@@ -6,7 +6,6 @@ import { FontIcon } from "react-toolbox/lib/font_icon";
 import AddBillFAB from "./AddBillFAB";
 import NobtItButtonTheme from "./NobtItButtonTheme.scss";
 import { Button } from "react-toolbox/lib/button/index";
-import HeadRoom from "react-headroom";
 import Amount from "../../../components/Amount/Amount";
 import BrandedAppBar from "../../../components/BrandedAppBar";
 import Feed from "./Feed/Feed";
@@ -16,31 +15,29 @@ export default class Nobt extends React.Component {
   render = () => {
     return (
       <div className={styles.nobt}>
-        <HeadRoom>
-          <BrandedAppBar />
+        <BrandedAppBar />
 
-          <div className={styles.overviewContainer}>
-            <div className={styles.nobtTitle}>{this.props.name}</div>
-            <div className={styles.nobtMetadata}>
-              <ul>
-                <li>
-                  <div><FontIcon value="payment" /><Amount value={this.props.total} /></div>
-                </li>
-                <li>
-                  <div><FontIcon value="group" />{this.props.members.length}</div>
-                </li>
-              </ul>
-            </div>
-            {(!this.props.isNobtEmpty) && (
-              <Button
-                label="Show balances"
-                primary
-                raised
-                onClick={() => LocationBuilder.fromWindow().push("balances").apply(this.props.push)}
-                theme={NobtItButtonTheme}
-              />)}
+        <div className={styles.overviewContainer}>
+          <div className={styles.nobtTitle}>{this.props.name}</div>
+          <div className={styles.nobtMetadata}>
+            <ul>
+              <li>
+                <div><FontIcon value="payment" /><Amount value={this.props.total} /></div>
+              </li>
+              <li>
+                <div><FontIcon value="group" />{this.props.members.length}</div>
+              </li>
+            </ul>
           </div>
-        </HeadRoom>
+          {(!this.props.isNobtEmpty) && (
+            <Button
+              label="Show balances"
+              primary
+              raised
+              onClick={() => LocationBuilder.fromWindow().push("balances").apply(this.props.push)}
+              theme={NobtItButtonTheme}
+            />)}
+        </div>
 
         {
           this.props.isNobtEmpty
