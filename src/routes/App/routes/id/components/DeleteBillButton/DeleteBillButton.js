4e38b195f257692a137f68f329e4de730490fdbf
Thomas Eizinger <thomas@nobt.io>
2018-03-16 00:06:26 +0100

    Remove unused component

--- src/routes/App/routes/id/components/DeleteBillButton/DeleteBillButton.js
+++
@@ -1,9 +0,0 @@
-import Button from "../../../../../../components/Button/Button";
-import React from "react";
-import styles from "./DeleteBillButton.scss"
-
-export default ({onClick}) => (
-  <div>
-    <Button theme={styles} label="Delete" icon="delete" onClick={onClick} raised/>
-  </div>
-)
