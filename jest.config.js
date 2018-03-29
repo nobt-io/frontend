6cd059fd2eb9fa4f7a2bb964bc4f4c4927866c7d
Thomas Eizinger <thomas@eizinger.io>
2018-03-29 22:45:45 +0200

    Move config from package.json into dedicated files

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"modulePaths": [
+		"src"
+	],
+	"moduleDirectories": [
+		"node_modules"
+	],
+	"moduleNameMapper": {
+		"^.+\\.scss$": "identity-obj-proxy",
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy"
+	},
+	"verbose": true
+}
