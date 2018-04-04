ebfbef3d7a6cae36a68cb9cb8cab0ecdb3b416c9
Thomas Eizinger <thomas@eizinger.io>
2018-04-04 18:55:19 +0200

    Directly export the root reducer
    As we no longer have async reducers (I think we never really had them),
    we can just export the root reducer right away instead of exporting a fn.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,15 +1,10 @@
 import { combineReducers } from "redux";
-import { routerReducer as router } from "react-router-redux";
+import { routerReducer } from "react-router-redux";
 import createNobtFormReducer from "routes/CreateNobt/modules/reducer";
 import appReducer from "routes/App/modules/reducers";
 
-const makeRootReducer = asyncReducers => {
-  return combineReducers({
-    createNobtForm: createNobtFormReducer,
-    App: appReducer,
-    router,
-    ...asyncReducers
-  });
-};
-
-export default makeRootReducer;
+export default combineReducers({
+  createNobtForm: createNobtFormReducer,
+  App: appReducer,
+  router: routerReducer
+});
