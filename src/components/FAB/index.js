8cfbdb11ea70b729017ab1f7753deb56e117e737
Thomas Eizinger <thomas@eizinger.io>
2018-04-15 21:34:48 +1000

    Fix linting issues by using proper ES6 export syntax

--- src/components/FAB/index.js
+++ src/components/FAB/index.js
@@ -1,13 +1,6 @@
-import FAB from "./FABMenu";
-import Item from "./Item";
-import Items from "./Items";
-import Button from "./MenuButton"
-import Label from "./Label";
-import Overlay from "./Overlay";
-
-exports.FABMenu = FAB;
-exports.Item = Item;
-exports.Items = Items;
-exports.MenuButton = Button;
-exports.Label = Label;
-exports.Overlay = Overlay;
+export { default as FABMenu } from "./FABMenu";
+export { default as Item } from "./Item";
+export { default as Items } from "./Items";
+export { default as MenuButton } from "./MenuButton"
+export { default as Label } from "./Label";
+export { default as Overlay } from "./Overlay";
