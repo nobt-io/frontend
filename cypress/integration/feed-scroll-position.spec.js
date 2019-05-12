20fc39726739d602e682752b93f53eb25f371c21
Thomas Eizinger <thomas@eizinger.io>
2019-05-12 13:58:11 +1000

    Add UI test for scroll position being restored
    This requires us to run the test in the latest chrome browser
    instead of electron.
    We use a custom assertion to check that the specific element is
    actually in the viewport.

---
+++ cypress/integration/feed-scroll-position.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+
+describe('The scroll position', function() {
+  beforeEach(() => {
+    cy.fixture("nobt-with-16-bills").as("nobt")
+  });
+
+  it('should be on the same after navigating to bill details and back', function() {
+
+    // Need to fix date otherwise would show different timestamps for the feed item
+    cy.clock(new Date(2019, 1, 5));
+
+    cy.server();
+    cy.route({
+      method: 'GET',
+      url: '/nobts/' + this.nobt.id,
+      status: 200,
+      response: '@nobt',
+    });
+    cy.visit(this.nobt.id);
+
+    cy.contains('li', 'The last bill').as("lastBill").should("exist").scrollIntoView();
+
+    cy.get("@lastBill").click();
+    cy.contains("David paid this bill.").should("exist");
+    cy.go("back");
+
+    cy.get("@lastBill").should("be.inViewport");
+  });
+});
--- cypress/run.sh
+++ cypress/run.sh
@@ -7,6 +7,6 @@ node_modules/.bin/ws -p 3000 -d dist --spa index.html &
 HTTP_SERVER_PID=$!
 
 sleep 1
-node_modules/.bin/cypress run
+node_modules/.bin/cypress run --browser chrome
 
 kill -9 ${HTTP_SERVER_PID}
