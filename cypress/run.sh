e71142ab7d49cdcac06c9c5615e8277c5f04ca04
Thomas Eizinger <thomas@eizinger.io>
2019-11-17 16:45:41 +1100

    Allow for passing of arguments to cypress in cypress/run.sh script

--- cypress/run.sh
+++ cypress/run.sh
@@ -6,7 +6,12 @@ set -e;
 node_modules/.bin/ws -p 3000 -d dist --spa index.html &
 HTTP_SERVER_PID=$!
 
-sleep 1
-node_modules/.bin/cypress run --browser chrome
+function kill_http_server() {
+  kill -9 ${HTTP_SERVER_PID}
+}
 
-kill -9 ${HTTP_SERVER_PID}
+trap kill_http_server EXIT
+
+sleep 1
+node_modules/.bin/cypress run "$@"
+
