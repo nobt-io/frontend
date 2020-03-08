d62f78ca5ee4df9013177edf3dfc743347e56818
Thomas Eizinger <thomas@eizinger.io>
2020-03-08 14:54:30 +1100

    Create tailwind config based on current color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        logo: ['Comfortaa'],
+      },
+      colors: {
+        green: {
+          default: '#2d978d',
+          light: '#34aca1',
+        },
+        black: {
+          default: '#353535',
+        },
+        grey: {
+          default: '#979797',
+          light: '#e3e3e3',
+        },
+        red: '#de3226',
+      },
+    },
   },
   variants: {},
   plugins: [],
