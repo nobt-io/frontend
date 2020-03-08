829667315f9d1e80bf5e6ede8ea2f93e53183303
Thomas Eizinger <thomas@eizinger.io>
2020-03-08 23:29:09 +1100

    Depend on font-awesome through npm

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,18 @@ module.exports = (_, argv) => {
             },
           ],
         },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name]_[hash:4].[ext]',
+                outputPath: 'fonts/',
+              },
+            },
+          ],
+        },
       ],
     },
     resolve: {
